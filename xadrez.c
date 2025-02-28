763c6b56952f50f8aa70850d21870330b6a7abf0
Rafaeldiniz-1904 <202502006403@alunos.estacio.br>
2025-02-28 16:21:13 +0000

    Criação das variaveis

--- xadrez.c
+++ xadrez.c
@@ -5,6 +5,15 @@
 // O objetivo é utilizar estruturas de repetição e funções para determinar os limites de movimentação dentro do jogo.
 
 int main() {
+
+    // Criação das variaveis.
+
+    int bispo = 5;
+    int torre = 5;
+    int rainha = 8;
+
+    // Movimentação da Torre
+    
     // Nível Novato - Movimentação das Peças
     // Sugestão: Declare variáveis constantes para representar o número de casas que cada peça pode se mover.
 
